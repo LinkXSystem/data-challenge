8348a6e31379cec9b78a853593ba1fbe6beed77f
zhi <linkasde_system@163.com>
2019-02-19 19:06:20 +0800

    :sparkles: 更新时间轴

--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import classes from '../../lib/classes';
+import classes from '@/lib/classes';
 
-export default ({ name }) => {
+import './style/index.scss';
+
+export default function Icon({ name }) {
   return <i className={classes(['linksystem', name])} />;
+}
+
+Icon.propTypes = {
+  name: PropTypes.string.isRequired,
 };
